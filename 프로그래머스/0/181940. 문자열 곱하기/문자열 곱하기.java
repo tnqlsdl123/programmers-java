f69195b61b924c307199758e4f0edd32bf41e96d
tnqlsdl123 <ace10433@gmail.com>
2026-09-02 14:30:08 +0900

    [level 0] Title: 문자열 곱하기, Time: 3.00 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int k) {
+        String answer = "";
+        int i = 0;
+        
+        while (i < k) {
+            answer += my_string;
+            i++;
+        }
+        
+        return answer;
+    }
+}
