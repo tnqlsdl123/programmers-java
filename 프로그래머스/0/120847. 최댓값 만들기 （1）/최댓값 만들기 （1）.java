b309ba8f83e0b72d2a3f7bce170ab22c676bbfc1
tnqlsdl123 <ace10433@gmail.com>
2026-09-10 23:46:51 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 3.47 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -1,20 +1,12 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
-        int max1 = 0;
-        int max2 = 0;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > max1) {
-                max2 = max1;
-                max1 = numbers[i];
-            } else if (numbers[i] > max2) {
-                max2 = numbers[i];
-            }
-        }
         
-        answer = max1 * max2;
-
+        Arrays.sort(numbers);
+        answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        
         return answer;
     }
 }
